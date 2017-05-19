15428fb683395aa9bd40b23395030a43a35e0820
zander312 <iskander93@gmail.com>
2017-05-18 22:33:45 -0400

    adds images

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
-    console.log("we are ready to go")
+	var imgArr = [
+	"<img class='mapImage' src='./assets/appellation-map.png'>",
+	"<img class='image' src='./assets/hill-of-hermitage.jpg' >",
+	"<img class='image' src='./assets/chap-4.jpg' >",
+	"<img class='image' src='./assets/581911.jpg' >"
+
+	]
+
+	// for(var i=0; i<imgArr.length; i++) {
+	// 	console.log(imgArr[i])
+	// 	i == imgArr.length - 1 ? i = 0 : null
+	// 	set
+	// }
+
+
+	function imageLoop(){
+		var count = 0
+		var interval = 20000
+		setInterval(function(){
+			$('.root').html(imgArr[count])
+			count++
+			count == imgArr.length ? count = 0 : null
+		}, interval)
+	}
+
+
+imageLoop()
 
 })
