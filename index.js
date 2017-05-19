b148fcf4de397d4caf878c578d6cfa88ea417aa7
zander312 <iskander93@gmail.com>
2017-05-19 15:55:51 -0400

    recursive loop

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
 	var imgArr = [
-	"<img class='mapImage' src='./assets/appellation-map.png'>",
+	"<img class='mapImage' src='./assets/image1.png'>",
 	"<img class='image' src='./assets/hill-of-hermitage.jpg' >",
-	"<img class='image' src='./assets/chap-4.jpg' >",
-	"<img class='image' src='./assets/581911.jpg' >"
+	// "<img class='image' src='./assets/chap-4.jpg' >",
+	"<img class='image' src='./assets/581911.jpg' >",
+	"<img class='image' src='./assets/image2.jpg'>",
+	"<img class='image' src='./assets/image3.jpg'>"
 
 	]
 
@@ -15,17 +17,43 @@ $(document).ready(function(){
 	// }
 
 
-	function imageLoop(){
-		var count = 0
-		var interval = 20000
-		setInterval(function(){
-			$('.root').html(imgArr[count])
-			count++
-			count == imgArr.length ? count = 0 : null
-		}, interval)
+	// function imageLoop(){
+	// 	var count = 0
+	// 	var interval = 2000
+	// 	setInterval(function(){
+	// 		$('.root').html(imgArr[count])
+	// 		count++
+	// 		count == imgArr.length ? count = 0 : null
+	// 	}, interval)
+	// }
+
+	// function imageLoop() {
+	// 	var count = 0
+	// 	var interval = 2000
+	// 	setInterval(function() {
+	// 		$('.root').html(imgArr[count])
+	// 		count++
+	// 		count == imgArr.length ? count = 0 : null
+	// 	}, interval)
+	// }
+
+	function timeoutLoop(imgIndex){
+		console.log(imgIndex)
+		$('.root').html(imgArr[imgIndex])
+		if(imgIndex == 0){
+			setTimeout(function(){
+				imgIndex == imgArr.length - 1 ? imgIndex = 0 : imgIndex++
+				timeoutLoop(imgIndex)
+			}, 60000)
+		}	else {
+			setTimeout(function(){
+				imgIndex == imgArr.length - 1 ? imgIndex = 0 : imgIndex++
+				timeoutLoop(imgIndex)
+			}, 20000)
+		}
 	}
 
+timeoutLoop(0)
 
-imageLoop()
 
 })
